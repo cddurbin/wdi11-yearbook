6038419973937bd347d38a743e0e9e47ee8cee0a
Colin Durbin <cddurbin@gmail.com>
2015-03-16 16:21:22 +0000

    Apply ng-repeat to students array.

---
+++ js/app.js
@@ -0,0 +1,39 @@
+var app = angular.module('wdiYearbook', []);
+
+app.controller('StudentsController', function($scope){
+  $scope.students = students;
+});
+
+
+
+
+
+
+students = [{
+  name: 'Kudus',
+  bio: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Commodi error fuga quae. Sed labore praesentium, est commodi eum dolorem natus neque inventore a ipsam, excepturi iusto. Voluptatem beatae iure eius!',
+  photo: 'images/Cute-Babies.jpg',
+  email: 'kudus@kudus.com',
+  github: 'github.com/kudus'
+},
+{
+  name: 'Colin',
+  bio: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Commodi error fuga quae. Sed labore praesentium, est commodi eum dolorem natus neque inventore a ipsam, excepturi iusto. Voluptatem beatae iure eius!',
+  photo: 'images/Cute-Babies.jpg',
+  email: 'colin@colin.com',
+  github: 'github.com/colin'
+},
+{
+  name: 'Betty',
+  bio: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Commodi error fuga quae. Sed labore praesentium, est commodi eum dolorem natus neque inventore a ipsam, excepturi iusto. Voluptatem beatae iure eius!',
+  photo: 'images/Cute-Babies.jpg',
+  email: 'betty@betty.com',
+  github: 'github.com/betty'
+},
+{
+  name: 'Allie',
+  bio: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Commodi error fuga quae. Sed labore praesentium, est commodi eum dolorem natus neque inventore a ipsam, excepturi iusto. Voluptatem beatae iure eius!',
+  photo: 'images/Cute-Babies.jpg',
+  email: 'allie@allie.com',
+  github: 'github.com/allie'
+}]
