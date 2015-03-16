b5d7d459c0570e69c5fcbba2fd87cfec058bff26
Colin Durbin <cddurbin@gmail.com>
2015-03-16 20:18:57 +0000

    Css styling.

--- js/app.js
+++ js/app.js
@@ -5,6 +5,16 @@ app.controller('StudentsController', function($scope){
   $scope.selectStudent = function(student){
     $scope.selectedStudent = student;
   }
+  $scope.class = "hide";
+  $scope.showMore = function() {
+    if ($scope.class === "show")
+      $scope.class = "hide";
+    else
+      $scope.class = "show";
+  };
+  $scope.closeModal = function() {
+    $scope.class = "hide";
+  }
 });
 
 
@@ -15,28 +25,28 @@ app.controller('StudentsController', function($scope){
 students = [{
   name: 'Kudus',
   bio: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Commodi error fuga quae. Sed labore praesentium, est commodi eum dolorem natus neque inventore a ipsam, excepturi iusto. Voluptatem beatae iure eius!',
-  photo: 'images/Cute-Babies.jpg',
+  photo: 'images/kudus2.jpg',
   email: 'kudus@kudus.com',
   github: 'github.com/kudus'
 },
 {
   name: 'Colin',
   bio: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Commodi error fuga quae. Sed labore praesentium, est commodi eum dolorem natus neque inventore a ipsam, excepturi iusto. Voluptatem beatae iure eius!',
-  photo: 'images/Cute-Babies.jpg',
+  photo: 'images/colin1.jpg',
   email: 'colin@colin.com',
   github: 'github.com/colin'
 },
 {
-  name: 'Betty',
+  name: 'Alex',
   bio: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Commodi error fuga quae. Sed labore praesentium, est commodi eum dolorem natus neque inventore a ipsam, excepturi iusto. Voluptatem beatae iure eius!',
-  photo: 'images/Cute-Babies.jpg',
-  email: 'betty@betty.com',
-  github: 'github.com/betty'
+  photo: 'images/alex1.jpg',
+  email: 'alex@alex.com',
+  github: 'github.com/alex'
 },
 {
   name: 'Allie',
   bio: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Commodi error fuga quae. Sed labore praesentium, est commodi eum dolorem natus neque inventore a ipsam, excepturi iusto. Voluptatem beatae iure eius!',
-  photo: 'images/Cute-Babies.jpg',
+  photo: 'images/allie1.jpg',
   email: 'allie@allie.com',
   github: 'github.com/allie'
 }]
